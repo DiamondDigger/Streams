c3164195c7d91d3dcaf88723152587c5c1f204d5
naglec13 <naglec13@gmail.com>
2020-04-05 16:49:27 +0300

    ..

--- src/by/mustWork_01/firstImpression.java
+++ src/by/mustWork_01/firstImpression.java
@@ -4,11 +4,17 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.security.cert.CollectionCertStoreParameters;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
 
 public class firstImpression {
 
@@ -29,14 +35,14 @@ public class firstImpression {
 
         Stream<Integer> streamFromIntegers = Stream.of(1,3,4,5,6,67,657);
 
-        System.out.println("1. Создание стрима из значений streamFromIntegers: "+streamFromIntegers.collect(Collectors.toList()));
+        System.out.println("1. Создание стрима из значений streamFromIntegers: "+streamFromIntegers.collect(toList()));
 
         //2. Создание стрима из массива
         //a
         Integer[] arrayInt = {1,2,3,4,5,6,7};
         Stream<Integer> streamFromArray = Arrays.stream(arrayInt);
 
-        System.out.println("2. Создание стрима из массива streamFromArray: "+ streamFromArray.collect(Collectors.toList()));
+        System.out.println("2. Создание стрима из массива streamFromArray: "+ streamFromArray.collect(toList()));
         //b
 
         //3. Создание стрима из файла (каждая запись в файле будет отдельной строкой в стриме)
@@ -45,12 +51,28 @@ public class firstImpression {
         fileWriter.write("This is a content of my new created file");
         Stream<String> streamFromFile = Stream.of(file.toString());
 
-        System.out.println("3. Создание стрима из файла (каждая запись в файле будет отдельной строкой в стриме) streamFromFile: "+ streamFromFile.collect(Collectors.toList()));
+        System.out.println("3. Создание стрима из файла (каждая запись в файле будет отдельной строкой в стриме) streamFromFile: "+ streamFromFile.collect(toList()));
 
 
         //4. Создание стрима из коллекции
+        List<String> list = new ArrayList<>();
+        list.add("Cat");
+        list.add("Milk");
+        list.add("Cow");
+        list.add("Horse");
+        list.add("Sand");
+        Stream<String> streamFromList = list.stream();
+
+        System.out.println("4. Создание стрима из коллекции streamFromList: "+ streamFromList.collect(toList()));
 
         //5. Создание числового стрима из строки
+
+        String sequence = "I don't know what to put here.";
+        IntStream streamFromSequence = sequence.chars();
+
+        System.out.print("5. Создание числового стрима из строки streamFromSequence: ");
+        streamFromSequence.forEach((e)-> System.out.print(e + ", "));
+
 
         //6. С помощью Stream.builder
 
@@ -79,9 +101,8 @@ public class firstImpression {
 
 
 
-
         // бесконечный stream через iterate
         Stream<Integer> newStream = Stream.iterate(1, n -> n * 5);
-        System.out.println("newStream : "+ newStream.limit(5).collect(Collectors.toList()));
+        System.out.println("newStream : "+ newStream.limit(5).collect(toList()));
     }
 }
