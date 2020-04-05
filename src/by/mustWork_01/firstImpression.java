70dd2103c49dd472e668cec43ef9a12e94f14a0c
naglec13 <naglec13@gmail.com>
2020-04-05 15:04:33 +0300

    ..

--- src/by/mustWork_01/firstImpression.java
+++ src/by/mustWork_01/firstImpression.java
@@ -1,6 +1,9 @@
 package by.mustWork_01;
 
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class firstImpression {
 
@@ -13,6 +16,12 @@ public class firstImpression {
                 //after sorted(): 0 20 24 40 78
                 .skip(1) // - считаются с 1-го элемента а не с 0-я
                 //after skip(1): 20 24 40 78
+                .limit(2)
+                //after limit(2): 20 24
                 .forEach(System.out::println);
+
+        // бесконечный stream через iterate
+        Stream<Integer> newStream = Stream.iterate(1, n -> n * 5);
+        System.out.println("newStream : "+ newStream.limit(5).collect(Collectors.toList()));
     }
 }
