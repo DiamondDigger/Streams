55a6eea7f9d965f9515224005888fd4d8a8f8d73
naglec13 <naglec13@gmail.com>
2020-04-06 23:16:55 +0300

    ..

--- src/by/mustWork_01/firstImpression.java
+++ src/by/mustWork_01/firstImpression.java
@@ -33,20 +33,20 @@ public class firstImpression {
 
         //1. Создание стрима из значений
 
-        Stream<Integer> streamFromIntegers = Stream.of(1,3,4,5,6,67,657);
+        Stream<Integer> streamFromIntegers = Stream.of(1, 3, 4, 5, 6, 67, 657);
 
-        System.out.println("1. Создание стрима из значений streamFromIntegers: "+streamFromIntegers.collect(toList()));
+        System.out.println("1. Создание стрима из значений streamFromIntegers: " + streamFromIntegers.collect(toList()));
 
         //2. Создание стрима из массива
         //a
-        Integer[] arrayInt = {1,2,3,4,5,6,7};
+        Integer[] arrayInt = {1, 2, 3, 4, 5, 6, 7};
         Stream<Integer> streamFromArray = Arrays.stream(arrayInt);
 
-        System.out.println("2. Создание стрима из массива streamFromArray: "+ streamFromArray.collect(toList()));
+        System.out.println("2. Создание стрима из массива streamFromArray: " + streamFromArray.collect(toList()));
         //b
         Stream<Integer> streamOfArray = Stream.of(arrayInt);
 
-        System.out.println("2. Создание стрима из массива streamOfArray: "+streamOfArray.collect(toList()));
+        System.out.println("2. Создание стрима из массива streamOfArray: " + streamOfArray.collect(toList()));
 
         //3. Создание стрима из файла (каждая запись в файле будет отдельной строкой в стриме)
 
@@ -57,7 +57,7 @@ public class firstImpression {
         fileWriter.write("\nthird line of the text!");
         fileWriter.close(); // ничего не запишет пока не закроем поток записи
         Stream<String> streamFromFile = Files.lines(Paths.get(file.getAbsolutePath()));
-        System.out.println("3. Создание стрима из файла (каждая запись в файле будет отдельной строкой в стриме) streamFromFile: "+streamFromFile.collect(toList()));
+        System.out.println("3. Создание стрима из файла (каждая запись в файле будет отдельной строкой в стриме) streamFromFile: " + streamFromFile.collect(toList()));
 
         //4. Создание стрима из коллекции
         List<String> list = new ArrayList<>();
@@ -68,7 +68,7 @@ public class firstImpression {
         list.add("Sand");
         Stream<String> streamFromList = list.stream();
 
-        System.out.println("4. Создание стрима из коллекции streamFromList: "+ streamFromList.collect(toList()));
+        System.out.println("4. Создание стрима из коллекции streamFromList: " + streamFromList.collect(toList()));
 
         //5. Создание числового стрима из строки
 
@@ -76,37 +76,41 @@ public class firstImpression {
         IntStream streamFromSequence = sequence.chars();
 
         System.out.print("5. Создание числового стрима из строки streamFromSequence: ");
-        streamFromSequence.forEach((e)-> System.out.print(e + ", "));
+        streamFromSequence.forEach((e) -> System.out.print(e + ", "));
         System.out.println();
 
         //6. С помощью Stream.builder
 
         Stream.Builder<String> builder = Stream.builder();
         Stream<String> streamFromBuilder = builder.add("a1").add("a2").add("a3").build();
-        System.out.println("6. Создание стрима с помощью Stream.builder: "+streamFromBuilder.collect(toList()));
-
+        System.out.println("6. Создание стрима с помощью Stream.builder: " + streamFromBuilder.collect(toList()));
 
 
         //7. Создание бесконечных стримов
         // С помощью Stream.iterate
 
-        Stream<Integer> streamIterate = Stream.iterate(1,x->x+2);
-        System.out.println("7. Создание бесконечных стримов c Stream.iterate: "+streamIterate.limit(10).collect(toList()));
+        Stream<Integer> streamIterate = Stream.iterate(1, x -> x + 2);
+        System.out.println("7. Создание бесконечных стримов c Stream.iterate: " + streamIterate.limit(10).collect(toList()));
 
         //8. С помощью Stream.generate
-        Stream<String> streamGenerate = Stream.generate(()->"abc");
-        System.out.println("8. Создание бесконечных стримов с помощью Stream.generate: "+streamGenerate.limit(5).collect(toList()));
+        Stream<String> streamGenerate = Stream.generate(() -> "abc");
+        System.out.println("8. Создание бесконечных стримов с помощью Stream.generate: " + streamGenerate.limit(5).collect(toList()));
 
         //9. Создать параллельный стрим из коллекции
         Collection<String> stringCollection = Collections.singletonList("String message");
         Stream<String> parallelStreamFromCollection = stringCollection.parallelStream();
 
-        System.out.println("9. Создать параллельный стрим из коллекции parallelStreamFromCollection: "+parallelStreamFromCollection.collect(Collectors.toList()));
+        System.out.println("9. Создать параллельный стрим из коллекции parallelStreamFromCollection: " + parallelStreamFromCollection.collect(Collectors.toList()));
 
         //10. Создать пустой стрим
 
-        Stream<String> streamEmpty =Stream.empty();
-        System.out.println("10. Создать пустой стрим: "+ streamEmpty.collect(toList()));
+        Stream<String> streamEmpty = Stream.empty();
+        System.out.println("10. Создать пустой стрим: " + streamEmpty.collect(toList()));
+
+        // пробные варианты
+        //Java 9 +
+        Stream.iterate((1), x -> x < 20, x -> x + 1)
+                .forEach(System.out::println);
 
     }
 }
